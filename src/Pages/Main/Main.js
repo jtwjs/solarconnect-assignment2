bd9a8690322547c3a30af3166ceddf6a5f82f481
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-08-17 01:09:07 +0900

    feat: Implement alert dialog

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,13 +1,18 @@
-import React, { useState, useCallback } from "react";
-import styled, { ThemeProvider, css } from 'styled-components/macro';
+import React, {useState, useCallback, useRef} from "react";
+import styled, {ThemeProvider, css} from 'styled-components/macro';
 
 import {theme} from 'Styles/Theme';
 import Timer from "Components/Timer";
 import InputField from "Components/InputField";
-import StartButton from "Components/StartButton";
+import Button from "Components/Button";
+import Alert from "Components/Alert";
 
 export default function Main() {
 	const [inputText, setInputText] = useState('');
+	const [unsortedNums, setUnsortedNums] = useState(null);
+	const [isOpenAlert, setIsOpenAlert] = useState(false);
+
+	const inputRef = useRef(null);
 
 	const handleInputWithNumericOnly = useCallback(e => {
 		const value = e.target.value;
@@ -15,36 +20,62 @@ export default function Main() {
 		regex.test(value) && setInputText(value);
 	}, []);
 
-	const handleStartButton = useCallback(() => {
-		console.log('click');
+	const handleInputBlur = useCallback(e => {
+		const value = e.target.value;
+		const lastChar = value.charAt(value.length - 1);
+		const regex = /[0-9]/;
+		!regex.test(lastChar) && setInputText(value.slice(0, -1));
 	},[]);
 
+	const handleSortingButton = useCallback(() => {
+		if (!inputText) {
+			setIsOpenAlert(true);
+			return;
+		}
+		const nums = inputText.split(',').map(num => parseInt(num));
+		setUnsortedNums(nums);
+		setInputText('');
+	}, [inputText]);
+
+	const handleCloseAlert = useCallback(() => {
+		setIsOpenAlert(false);
+		inputRef.current.focus();
+	}, []);
+
 	return (
-		<ThemeProvider theme={theme}>
+	 	<ThemeProvider theme={theme}>
 			<StyledMain>
 				<StyledTitle>Sorting Machine</StyledTitle>
 				<StyledSection>
 					<Timer kind="KOREA"/>
 					<InputField
-						inputText={inputText}
-						handleInputText={handleInputWithNumericOnly}
+						ref={inputRef}
+						value={inputText}
+						onChange={handleInputWithNumericOnly}
+						onBlur={handleInputBlur}
+						placeholder="ex: 1,2,3,4"
 					/>
-					<StyledStartButton clickHandler={handleStartButton} />
+					<SortingButton clickHandler={handleSortingButton}>Sorting</SortingButton>
 					<Timer kind="USA"/>
 				</StyledSection>
 			</StyledMain>
+		  <Alert
+			  isOpen={isOpenAlert}
+			  text="😅 값을 입력해주세요."
+			  closeHandler={handleCloseAlert}
+		  />
 		</ThemeProvider>
 	);
 }
 
 const StyledMain = styled.main`
-	width: 100%;
-	padding-bottom: 60px;
-	background-color: ${({theme}) => theme.color.background};
-	
-	@media screen and ${({theme}) => theme.device.tablet} {
+  width: 100%;
+  padding-bottom: 60px;
+  background-color: ${({theme}) => theme.color.background};
+
+  @media screen and ${({theme}) => theme.device.tablet} {
     padding: 0 ${({theme}) => theme.layout.md_margin} ${({theme}) => theme.layout.md_margin};
-	}
+  }
 
   @media screen and ${({theme}) => theme.device.mobile} {
     padding: 0 ${({theme}) => theme.layout.sm_margin} ${({theme}) => theme.layout.sm_margin};
@@ -52,12 +83,12 @@ const StyledMain = styled.main`
 `;
 
 const responsiveMixin = css`
-  @media screen and ${({ theme }) => theme.device.tablet} {
+  @media screen and ${({theme}) => theme.device.tablet} {
     max-width: ${({theme}) => theme.layout.md_max_container};
-    
+
   }
 
-  @media screen and ${({ theme }) => theme.device.mobile} {
+  @media screen and ${({theme}) => theme.device.mobile} {
     max-width: unset;
     width: 100%;
   }
@@ -65,29 +96,41 @@ const responsiveMixin = css`
 
 const StyledTitle = styled.h1`
   max-width: ${({theme}) => theme.layout.lg_max_container};
-	margin: 0 auto;
-	font-size: 30px;
-	font-weight: 700;
-	line-height: 2;
-	color: ${({theme}) => theme.color.blueGrey};
+  margin: 0 auto;
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 2;
+  color: ${({theme}) => theme.color.blueGrey};
 
   ${responsiveMixin}
 `
 
 const StyledSection = styled.section`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   max-width: ${({theme}) => theme.layout.lg_max_container};
-	height: 100vh;
-	margin: 0 auto;
-	padding: 30px 0;
-	border-radius: 20px;
-	background-color: ${({theme}) => theme.color.white};
+  height: 100vh;
+  margin: 0 auto;
+  padding: 30px 0;
+  border-radius: 20px;
+  background-color: ${({theme}) => theme.color.white};
 
   ${responsiveMixin}
 `;
 
-const StyledStartButton = styled(StartButton)`
-	margin-bottom: 50px;
+const SortingButton = styled(Button)`
+  margin-bottom: 50px;
+
+  @media screen and ${({theme}) => theme.device.tablet} {
+    &:not(:last-child) {
+      margin-bottom: 40px;
+    }
+  }
+
+  @media screen and ${({theme}) => theme.device.mobile} {
+    &:not(:last-child) {
+      margin-bottom: 30px;
+    }
+  }
 `
